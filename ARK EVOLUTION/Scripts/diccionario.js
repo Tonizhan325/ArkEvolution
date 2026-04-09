714dd58564df2bffc4ff70af01f504df3ad419af
Toni <lightplayer@gmail.com>
2026-04-09 11:12:02 +0200

    Eventos manejadores añadidos a Armas divinas, Dinosaurios y Niveles de poderes

--- ARK EVOLUTION/Scripts/diccionario.js
+++ ARK EVOLUTION/Scripts/diccionario.js
@@ -69,3 +69,577 @@ if (boton_volver_diccionario_interior != null){
         window.location.href = "../index.html"
     })
 }
+
+//ARMAS DIVINAS
+
+let boton_arena_x = document.getElementById("arena_x");
+
+if (boton_arena_x != null){
+    boton_arena_x.addEventListener("click", function(){
+        window.location.href = "arena_x.html"
+    })
+}
+
+let boton_emberius = document.getElementById("emberius");
+
+if (boton_emberius != null){
+    boton_emberius.addEventListener("click", function(){
+        window.location.href = "emberius.html"
+    })
+}
+
+let boton_arco_mek = document.getElementById("arco_mek");
+
+if (boton_arco_mek != null){
+    boton_arco_mek.addEventListener("click", function(){
+        window.location.href = "arco_mek.html"
+    })
+}
+
+let boton_devoraalmas = document.getElementById("devoraalmas");
+
+if (boton_devoraalmas != null){
+    boton_devoraalmas.addEventListener("click", function(){
+        window.location.href = "devoraalmas.html"
+    })
+}
+
+let boton_counter = document.getElementById("counter");
+
+if (boton_counter != null){
+    boton_counter.addEventListener("click", function(){
+        window.location.href = "counter.html"
+    })
+}
+
+let boton_masamune = document.getElementById("masamune");
+
+if (boton_masamune != null){
+    boton_masamune.addEventListener("click", function(){
+        window.location.href = "masamune.html"
+    })
+}
+
+let boton_sveror = document.getElementById("sveror");
+
+if (boton_sveror != null){
+    boton_sveror.addEventListener("click", function(){
+        window.location.href = "sveror.html"
+    })
+}
+
+let boton_svalin = document.getElementById("svalin");
+
+if (boton_svalin != null){
+    boton_svalin.addEventListener("click", function(){
+        window.location.href = "svalin.html"
+    })
+}
+
+let boton_gungnir = document.getElementById("gungnir");
+
+if (boton_gungnir != null){
+    boton_gungnir.addEventListener("click", function(){
+        window.location.href = "gungnir.html"
+    })
+}
+
+let boton_longino = document.getElementById("longino");
+
+if (boton_longino != null){
+    boton_longino.addEventListener("click", function(){
+        window.location.href = "longino.html"
+    })
+}
+
+//DINOSAURIOS
+
+let boton_rex = document.getElementById("rex");
+
+if (boton_rex != null){
+    boton_rex.addEventListener("click", function(){
+        window.location.href = "rex.html"
+    })
+}
+
+let boton_parasaurio = document.getElementById("parasaurio");
+
+if (boton_parasaurio != null){
+    boton_parasaurio.addEventListener("click", function(){
+        window.location.href = "parasaurio.html"
+    })
+}
+
+let boton_raptor = document.getElementById("raptor");
+
+if (boton_raptor != null){
+    boton_raptor.addEventListener("click", function(){
+        window.location.href = "raptor.html"
+    })
+}
+
+let boton_ave_de_terror = document.getElementById("ave_de_terror");
+
+if (boton_ave_de_terror != null){
+    boton_ave_de_terror.addEventListener("click", function(){
+        window.location.href = "ave_de_terror.html"
+    })
+}
+
+let boton_carnotauro = document.getElementById("carnotauro");
+
+if (boton_carnotauro != null){
+    boton_carnotauro.addEventListener("click", function(){
+        window.location.href = "carnotauro.html"
+    })
+}
+
+let boton_grifo = document.getElementById("grifo");
+
+if (boton_grifo != null){
+    boton_grifo.addEventListener("click", function(){
+        window.location.href = "grifo.html"
+    })
+}
+
+let boton_triceratop = document.getElementById("triceratop");
+
+if (boton_triceratop != null){
+    boton_triceratop.addEventListener("click", function(){
+        window.location.href = "triceratop.html"
+    })
+}
+
+let boton_araneo = document.getElementById("araneo");
+
+if (boton_araneo != null){
+    boton_araneo.addEventListener("click", function(){
+        window.location.href = "araneo.html"
+    })
+}
+
+let boton_sanguijuela = document.getElementById("sanguijuela");
+
+if (boton_sanguijuela != null){
+    boton_sanguijuela.addEventListener("click", function(){
+        window.location.href = "sanguijuela.html"
+    })
+}
+
+let boton_sarcocuchus = document.getElementById("sarcocuchus");
+
+if (boton_sarcocuchus != null){
+    boton_sarcocuchus.addEventListener("click", function(){
+        window.location.href = "sarcocuchus.html"
+    })
+}
+
+let boton_giganotosaurio = document.getElementById("giganotosaurio");
+
+if (boton_giganotosaurio != null){
+    boton_giganotosaurio.addEventListener("click", function(){
+        window.location.href = "giganotosaurio.html"
+    })
+}
+
+let boton_dragon = document.getElementById("dragon");
+
+if (boton_dragon != null){
+    boton_dragon.addEventListener("click", function(){
+        window.location.href = "dragon.html"
+    })
+}
+
+let boton_pteranodon = document.getElementById("pteranodon");
+
+if (boton_pteranodon != null){
+    boton_pteranodon.addEventListener("click", function(){
+        window.location.href = "pteranodon.html"
+    })
+}
+
+let boton_compy = document.getElementById("compy");
+
+if (boton_compy != null){
+    boton_compy.addEventListener("click", function(){
+        window.location.href = "compy.html"
+    })
+}
+
+let boton_espinosaurio = document.getElementById("espinosaurio");
+
+if (boton_espinosaurio != null){
+    boton_espinosaurio.addEventListener("click", function(){
+        window.location.href = "espinosaurio.html"
+    })
+}
+
+let boton_diplodocus = document.getElementById("diplodocus");
+
+if (boton_diplodocus != null){
+    boton_diplodocus.addEventListener("click", function(){
+        window.location.href = "diplodocus.html"
+    })
+}
+
+let boton_celacanto = document.getElementById("celacanto");
+
+if (boton_celacanto != null){
+    boton_celacanto.addEventListener("click", function(){
+        window.location.href = "celacanto.html"
+    })
+}
+
+let boton_salmon_diente_de_sable = document.getElementById("salmon_diente_de_sable");
+
+if (boton_salmon_diente_de_sable != null){
+    boton_salmon_diente_de_sable.addEventListener("click", function(){
+        window.location.href = "salmon_dientes_de_sable.html"
+    })
+}
+
+let boton_dodo = document.getElementById("dodo");
+
+if (boton_dodo != null){
+    boton_dodo.addEventListener("click", function(){
+        window.location.href = "dodo.html"
+    })
+}
+
+let boton_megalodon = document.getElementById("megalodon");
+
+if (boton_megalodon != null){
+    boton_megalodon.addEventListener("click", function(){
+        window.location.href = "megalodon.html"
+    })
+}
+
+let boton_thylacoleo = document.getElementById("thylacoleo");
+
+if (boton_thylacoleo != null){
+    boton_thylacoleo.addEventListener("click", function(){
+        window.location.href = "thylacoleo.html"
+    })
+}
+
+let boton_alosaurio = document.getElementById("alosaurio");
+
+if (boton_alosaurio != null){
+    boton_alosaurio.addEventListener("click", function(){
+        window.location.href = "alosaurio.html"
+    })
+}
+
+let boton_guiverno = document.getElementById("guiverno");
+
+if (boton_guiverno != null){
+    boton_guiverno.addEventListener("click", function(){
+        window.location.href = "guiverno.html"
+    })
+}
+
+let boton_dilofosaurio = document.getElementById("dilofosaurio");
+
+if (boton_dilofosaurio != null){
+    boton_dilofosaurio.addEventListener("click", function(){
+        window.location.href = "dilofosaurio.html"
+    })
+}
+
+let boton_quetzal = document.getElementById("quetzal");
+
+if (boton_quetzal != null){
+    boton_quetzal.addEventListener("click", function(){
+        window.location.href = "quetzal.html"
+    })
+}
+
+let boton_gallimimus = document.getElementById("gallimimus");
+
+if (boton_gallimimus != null){
+    boton_galliminus.addEventListener("click", function(){
+        window.location.href = "gallimimus.html"
+    })
+}
+
+let boton_titanosaurio = document.getElementById("titanosaurio");
+
+if (boton_titanosaurio != null){
+    boton_titanosaurio.addEventListener("click", function(){
+        window.location.href = "titanosaurio.html"
+    })
+}
+
+let boton_golem_de_cristal = document.getElementById("golem_de_cristal");
+
+if (boton_golem_de_cristal != null){
+    boton_golem_de_cristal.addEventListener("click", function(){
+        window.location.href = "golem_cristal.html"
+    })
+}
+
+let boton_guiverno_de_cristal = document.getElementById("guiverno_de_cristal");
+
+if (boton_guiverno_de_cristal != null){
+    boton_guiverno_de_cristal.addEventListener("click", function(){
+        window.location.href = "guiverno_cristal.html"
+    })
+}
+
+let boton_leedsichthys = document.getElementById("leedsichthys");
+
+if (boton_leedsichthys != null){
+    boton_leedsichthys.addEventListener("click", function(){
+        window.location.href = "leedsichthys.html"
+    })
+}
+
+let boton_mosasaurio = document.getElementById("mosasaurio");
+
+if (boton_mosasaurio != null){
+    boton_mosasaurio.addEventListener("click", function(){
+        window.location.href = "mosasaurio.html"
+    })
+}
+
+let boton_fenix = document.getElementById("fenix");
+
+if (boton_fenix != null){
+    boton_fenix.addEventListener("click", function(){
+        window.location.href = "fenix.html"
+    })
+}
+
+let boton_daeodon = document.getElementById("daeodon");
+
+if (boton_daeodon != null){
+    boton_daeodon.addEventListener("click", function(){
+        window.location.href = "daeodon.html"
+    })
+}
+
+let boton_yutyrannus = document.getElementById("yutyrannus");
+
+if (boton_yutyrannus != null){
+    boton_yutyrannus.addEventListener("click", function(){
+        window.location.href = "yutyrannus.html"
+    })
+}
+
+let boton_ovis = document.getElementById("ovis");
+
+if (boton_ovis != null){
+    boton_ovis.addEventListener("click", function(){
+        window.location.href = "ovis.html"
+    })
+}
+
+let boton_plesiosaurio = document.getElementById("plesiosaurio");
+
+if (boton_plesiosaurio != null){
+    boton_plesiosaurio.addEventListener("click", function(){
+        window.location.href = "plesiosaurio.html"
+    })
+}
+
+let boton_lystrosaurio = document.getElementById("lystrosaurio");
+
+if (boton_lystrosaurio != null){
+    boton_lystrosaurio.addEventListener("click", function(){
+        window.location.href = "lystrosaurio.html"
+    })
+}
+
+let boton_megaphitecus = document.getElementById("megaphitecus");
+
+if (boton_megaphitecus != null){
+    boton_megaphitecus.addEventListener("click", function(){
+        window.location.href = "megaphitecus.html"
+    })
+}
+
+let boton_broodmother = document.getElementById("broodmother");
+
+if (boton_broodmother != null){
+    boton_broodmother.addEventListener("click", function(){
+        window.location.href = "broodmother.html"
+    })
+}
+
+let boton_leon_diente_de_sable = document.getElementById("leon_diente_de_sable");
+
+if (boton_leon_diente_de_sable != null){
+    boton_leon_diente_de_sable.addEventListener("click", function(){
+        window.location.href = "leon_dientes_de_sable.html"
+    })
+}
+
+let boton_titanoboa = document.getElementById("titanoboa");
+
+if (boton_titanoboa != null){
+    boton_titanoboa.addEventListener("click", function(){
+        window.location.href = "titanoboa.html"
+    })
+}
+
+let boton_manticora = document.getElementById("manticora");
+
+if (boton_manticora != null){
+    boton_manticora.addEventListener("click", function(){
+        window.location.href = "manticora.html"
+    })
+}
+
+let boton_equus = document.getElementById("equus");
+
+if (boton_equus != null){
+    boton_equus.addEventListener("click", function(){
+        window.location.href = "equus.html"
+    })
+}
+
+let boton_gusano_de_la_muerte = document.getElementById("gusano_de_la_muerte");
+
+if (boton_gusano_de_la_muerte != null){
+    boton_gusano_de_la_muerte.addEventListener("click", function(){
+        window.location.href = "gusano_muerte.html"
+    })
+}
+
+let boton_morellatops = document.getElementById("morellatops");
+
+if (boton_morellatops != null){
+    boton_morellatops.addEventListener("click", function(){
+        window.location.href = "morellatops.html"
+    })
+}
+
+let boton_argentavis = document.getElementById("argentavis");
+
+if (boton_argentavis != null){
+    boton_argentavis.addEventListener("click", function(){
+        window.location.href = "argentavis.html"
+    })
+}
+
+let boton_estegosaurio = document.getElementById("estegosaurio");
+
+if (boton_estegosaurio != null){
+    boton_estegosaurio.addEventListener("click", function(){
+        window.location.href = "estegosaurio.html"
+    })
+}
+
+let boton_polilla = document.getElementById("polilla");
+
+if (boton_polilla != null){
+    boton_polilla.addEventListener("click", function(){
+        window.location.href = "polilla.html"
+    })
+}
+
+let boton_onyx = document.getElementById("onyx");
+
+if (boton_onyx != null){
+    boton_onyx.addEventListener("click", function(){
+        window.location.href = "onyx.html"
+    })
+}
+
+let boton_gigaphitecus = document.getElementById("gigaphitecus");
+
+if (boton_gigaphitecus != null){
+    boton_gigaphitecus.addEventListener("click", function(){
+        window.location.href = "gigaphitecus.html"
+    })
+}
+
+let boton_mantis_gigante = document.getElementById("mantis_gigante");
+
+if (boton_mantis_gigante != null){
+    boton_mantis_gigante.addEventListener("click", function(){
+        window.location.href = "mantis_gigante.html"
+    })
+}
+
+//NIVELES DE PODERES
+
+let boton_niveles_de_poderes_t1 = document.getElementById("niveles_de_poderes_t1");
+
+if (boton_niveles_de_poderes_t1 != null){
+    boton_niveles_de_poderes_t1.addEventListener("click", function(){
+        window.location.href = "Temporada 1/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t2 = document.getElementById("niveles_de_poderes_t2");
+
+if (boton_niveles_de_poderes_t2 != null){
+    boton_niveles_de_poderes_t2.addEventListener("click", function(){
+        window.location.href = "Temporada 2/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t3 = document.getElementById("niveles_de_poderes_t3");
+
+if (boton_niveles_de_poderes_t3 != null){
+    boton_niveles_de_poderes_t3.addEventListener("click", function(){
+        window.location.href = "Temporada 3/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t4 = document.getElementById("niveles_de_poderes_t4");
+
+if (boton_niveles_de_poderes_t4 != null){
+    boton_niveles_de_poderes_t4.addEventListener("click", function(){
+        window.location.href = "Temporada 4/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t5 = document.getElementById("niveles_de_poderes_t5");
+
+if (boton_niveles_de_poderes_t5 != null){
+    boton_niveles_de_poderes_t5.addEventListener("click", function(){
+        window.location.href = "Temporada 5/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t6 = document.getElementById("niveles_de_poderes_t6");
+
+if (boton_niveles_de_poderes_t6 != null){
+    boton_niveles_de_poderes_t6.addEventListener("click", function(){
+        window.location.href = "Temporada 6/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t7 = document.getElementById("niveles_de_poderes_t7");
+
+if (boton_niveles_de_poderes_t7 != null){
+    boton_niveles_de_poderes_t7.addEventListener("click", function(){
+        window.location.href = "Temporada 7/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t8 = document.getElementById("niveles_de_poderes_t8");
+
+if (boton_niveles_de_poderes_t8 != null){
+    boton_niveles_de_poderes_t8.addEventListener("click", function(){
+        window.location.href = "Temporada 8/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t9 = document.getElementById("niveles_de_poderes_t9");
+
+if (boton_niveles_de_poderes_t9 != null){
+    boton_niveles_de_poderes_t9.addEventListener("click", function(){
+        window.location.href = "Temporada 9/index.html"
+    })
+}
+
+let boton_niveles_de_poderes_t10 = document.getElementById("niveles_de_poderes_t10");
+
+if (boton_niveles_de_poderes_t10 != null){
+    boton_niveles_de_poderes_t10.addEventListener("click", function(){
+        window.location.href = "Temporada 10/index.html"
+    })
+}
